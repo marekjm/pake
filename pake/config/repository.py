282347f5921b6593a769fbd5602a7a3f913404af
Marek Marecki <marekjm@taistelu.com>
2013-08-01 20:41:18 +0200

    Backend is now able to create packages

--- pake/config/repository.py
+++ pake/config/repository.py
@@ -73,3 +73,22 @@ class Dependencies(base.Config):
             del self.content[n]
             self.write()
         return n
+
+
+class Files(base.Config):
+    """Interface to package's `files.json` config file.
+    """
+    name = 'files.json'
+    default = []
+    content = []
+
+    def add(self, string):
+        """Adds file to the list.
+        """
+        self.content.append(string)
+        self.write()
+
+    def remove(self, string):
+        """Removes file from the list.
+        """
+        self.content.pop(self.content.index(string))
--- pake/repository.py
+++ pake/repository.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import tarfile
 
 from pake import config
 
@@ -22,7 +23,7 @@ def makedirs(root):
     :param root: root of the repository
     :type root: str
     """
-    subdirectories = ['versions']
+    subdirectories = ['packages', 'versions']
     os.mkdir(root)
     for name in subdirectories:
         os.mkdir(os.path.join(root, name))
@@ -36,3 +37,20 @@ def makeconfig(root):
     """
     config.repository.Meta(root).reset()
     config.repository.Dependencies(root).reset()
+    config.repository.Files(root).reset()
+
+
+def makepackage(root, overwrite=False):
+    """Makes a package from files contained in repository.
+
+    :param root: root for the repository
+    """
+    meta = config.repository.Meta(root)
+    files = config.repository.Files(root)
+    if not files.content: raise Exception('package will not be created: empty file list')
+    name = '{0}-{1}.tar.xz'.format(meta['name'], meta['version'])
+    path = os.path.join(root, name)
+    if not overwrite and os.path.isfile(path): raise FileExistsError(path)
+    archieve = tarfile.TarFile.xzopen(name=path, mode='w')
+    for f in files: archieve.add(f)
+    archives.close()
