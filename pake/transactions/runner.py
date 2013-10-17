68ef39e8ca5431d0d4d60fdc64d9535f8361af66
Marek Marecki <marekjm@taistelu.com>
2013-10-17 15:50:51 +0200

    Added skeleton for public API of runner object

--- pake/transactions/runner.py
+++ pake/transactions/runner.py
@@ -1,4 +1,23 @@
 #!/sur/bin/env python3
 
-"""This file contains PAKE transactions interpreter.
+"""This file contains PAKE transactions runner.
 """
+
+
+class Runner():
+    """This object implements logic needed to run PAKE
+    requests - request is a middle-form of a single line of
+    transaction file.
+    """
+    def __init__(self, reqs):
+        self._reqs = reqs
+
+    def finalize(self):
+        """Finalize data in the transaction e.g. fill missing data.
+        """
+        return self
+
+    def run(self):
+        """Call this method to run the transaction.
+        """
+        pass
