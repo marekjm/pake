74e95b30a5436a9b5affd78cd410a3938cac5aa8
Marek Marecki <marekjm@taistelu.com>
2013-11-09 17:02:30 +0100

    Small refactoring in new test suite, tests for node configuration API moved to new test suite

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,3 +2,35 @@
 
 """Helper methods for PAKE test suite.
 """
+
+
+import os
+
+
+import pake
+
+
+def gennode(path):
+    """Generates a node in the test directory.
+    """
+    pake.node.manager.makedirs(path)
+    pake.node.manager.makeconfig(path)
+
+
+def rmnode(path):
+    """Removes node from path.
+    """
+    pake.node.manager.remove(path)
+
+
+def gennest(path):
+    """Initialize nest in given path.
+    """
+    pake.nest.manager.makedirs(root=path)
+    pake.nest.manager.makeconfig(root=path)
+
+
+def rmnest(path):
+    """Removes nest from given path.
+    """
+    pake.nest.manager.remove(root=path)
--- tests/system.py
+++ tests/system.py
@@ -26,32 +26,6 @@ test_nest_root = testdir + '/.pakenest'
 
 
 # Test environment setup
-def gennode(path):
-    """Generates a node in the test directory.
-    """
-    pake.node.manager.makedirs(path)
-    pake.node.manager.makeconfig(path)
-
-
-def rmnode(path):
-    """Removes node from path.
-    """
-    pake.node.manager.remove(path)
-
-
-def gennest(path):
-    """Initialize nest in given path.
-    """
-    pake.nest.manager.makedirs(root=path)
-    pake.nest.manager.makeconfig(root=path)
-
-
-def rmnest(path):
-    """Removes nest from given path.
-    """
-    pake.nest.manager.remove(root=path)
-
-
 def prepare(testdir):
     """Prepares test environment.
     """
@@ -59,75 +33,269 @@ def prepare(testdir):
         print('* creating test directory: {0}'.format(testdir))
         os.mkdir(testdir)
     if os.path.isdir(test_node_root):
-        print('* removing old test node: {0}'.format(test_node_root))
+        print('* removing leftover test node: {0}'.format(test_node_root))
         shutil.rmtree(test_node_root)
     if os.path.isdir(test_nest_root):
         print('* removing old test nest: {0}'.format(test_nest_root))
         shutil.rmtree(test_nest_root)
-    print('* generating new node...')
-    gennode(testdir)
-    print('* generating new nest...')
-    gennest(testdir)
     print()  # line break between prepare()'s output and test suite output
 
 
+# Node related tests
 class NodeManagerTests(unittest.TestCase):
     def testNodeManagerDirectoriesWriting(self):
         """This test checks for correct initialization of all required directories.
         """
+        # preparation
+        helpers.gennode(testdir)
         ifstream = open('./env/node/required/directories.json')
         directories = json.loads(ifstream.read())
         ifstream.close()
+        # test logic
         self.assertIn('.pakenode', os.listdir(testdir))
         for d in directories:
             path = os.path.join(test_node_root, d)
             self.assertEqual(True, os.path.isdir(path))
+        # cleanup
+        helpers.rmnode(testdir)
 
     def testNodeManagerConfigWriting(self):
         """This test checks for correct intialization of all required config files.
         """
-        # (filename, desired_content)
-        configs = [ ('meta.json', {}),
+        # preparation
+        helpers.gennode(testdir)
+        configs = [ ('meta.json', {}),      # (filename, desired_content)
                     ('pushers.json', []),
                     ('aliens.json', {}),
                     ('nests.json', {}),
                     ]
+        # test logic
         self.assertIn('.pakenode', os.listdir(testdir))
         for f, desired in configs:
             path = os.path.join(test_node_root, f)
             ifstream = open(path, 'r')
             self.assertEqual(desired, json.loads(ifstream.read()))
             ifstream.close()
+        # cleanup
+        helpers.rmnode(testdir)
 
     def testNodeManagerRemovingNode(self):
         """This test checks if node gets correctly deleted.
         """
+        # preparation
+        helpers.gennode(testdir)
+        # code logic & cleanup - in this test it's the same
         pake.node.manager.remove(root=testdir)
         self.assertNotIn('.pakenode', os.listdir(testdir))
 
 
+class NodeConfigurationTests(unittest.TestCase):
+    """Code for Meta() is shared between node and
+    nest configuration interface so it's only tested here.
+
+    Any tests passing for node will also pass for nests.
+    """
+    def testSettingKeyInMeta(self):
+        helpers.gennode(testdir)
+        # test logic
+        pake.config.node.Meta(test_node_root).set('foo', 'bar').write()
+        self.assertEqual(pake.config.node.Meta(test_node_root).get('foo'), 'bar')
+        pake.config.node.Meta(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testRemovingKeyFromMeta(self):
+        helpers.gennode(testdir)
+        # test logic
+        pake.config.node.Meta(test_node_root).set('foo', 'bar').write().remove('foo').write()
+        self.assertEqual(dict(pake.config.node.Meta(test_node_root)), {})
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testGettingMetaKeys(self):
+        helpers.gennode(testdir)
+        # test logic
+        pake.config.node.Meta(test_node_root).set('foo', 0).set('bar', 1).set('baz', 2).write()
+        self.assertEqual(['bar', 'baz', 'foo'], sorted(pake.config.node.Meta(test_node_root).keys()))
+        pake.config.node.Meta(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testAddingPusher(self):
+        helpers.gennode(testdir)
+        # test logic
+        pusher = {'url': 'http://pake.example.com', 'host': 'example.com', 'cwd': '/domains/example.com/public_html/pake'}
+        pake.config.node.Pushers(test_node_root).add(**pusher).write()
+        self.assertIn(pusher, pake.config.node.Pushers(test_node_root))
+        pake.config.node.Pushers(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testRemovingPusher(self):
+        helpers.gennode(testdir)
+        # test logic
+        pusher = {'url': 'http://pake.example.com', 'host': 'example.com', 'cwd': '/domains/example.com/public_html/pake'}
+        pake.config.node.Pushers(test_node_root).add(**pusher).write()
+        pake.config.node.Pushers(test_node_root).remove(url='http://pake.example.com').write()
+        self.assertNotIn(pusher, pake.config.node.Pushers(test_node_root))
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testGettingPusher(self):
+        helpers.gennode(testdir)
+        # test logic
+        pusher = {'url': 'http://pake.example.com', 'host': 'example.com', 'cwd': '/domains/example.com/public_html/pake'}
+        pake.config.node.Pushers(test_node_root).add(**pusher).write()
+        self.assertEqual(pusher, pake.config.node.Pushers(test_node_root).get(url='http://pake.example.com'))
+        pake.config.node.Pushers(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testCheckingForURLInPushers(self):
+        helpers.gennode(testdir)
+        # test logic
+        pusher = {'url': 'http://pake.example.com', 'host': 'example.com', 'cwd': '/domains/example.com/public_html/pake'}
+        pake.config.node.Pushers(test_node_root).add(**pusher).write()
+        self.assertEqual(True, pake.config.node.Pushers(test_node_root).hasurl('http://pake.example.com'))
+        pake.config.node.Pushers(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testAddingAlien(self):
+        helpers.gennode(testdir)
+        # test logic
+        alien = {'url': 'http://alien.example.com', 'mirrors': [], 'meta': {}}
+        pake.config.node.Aliens(test_node_root).set(**alien).write()
+        self.assertIn('http://alien.example.com', pake.config.node.Aliens(test_node_root))
+        pake.config.node.Aliens(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testRemovingAlien(self):
+        helpers.gennode(testdir)
+        # test logic
+        alien = {'url': 'http://alien.example.com', 'mirrors': [], 'meta': {}}
+        pake.config.node.Aliens(test_node_root).set(**alien).write()
+        pake.config.node.Aliens(test_node_root).remove(alien['url']).write()
+        self.assertNotIn('http://alien.example.com', pake.config.node.Aliens(test_node_root))
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testGettingAlien(self):
+        helpers.gennode(testdir)
+        # test logic
+        alien = {'url': 'http://alien.example.com', 'mirrors': [], 'meta': {}}
+        pake.config.node.Aliens(test_node_root).set(**alien).write()
+        del alien['url']
+        self.assertEqual(alien, pake.config.node.Aliens(test_node_root).get('http://alien.example.com'))
+        pake.config.node.Aliens(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testListingAlienURLs(self):
+        helpers.gennode(testdir)
+        # test logic
+        foo = {'url': 'http://alien.example.com', 'mirrors': [], 'meta': {}}
+        bar = {'url': 'http://alien.example.net', 'mirrors': [], 'meta': {}}
+        baz = {'url': 'http://alien.example.org', 'mirrors': [], 'meta': {}}
+        pake.config.node.Aliens(test_node_root).set(**foo).set(**bar).set(**baz).write()
+        self.assertEqual(['http://alien.example.com', 'http://alien.example.net', 'http://alien.example.org'],
+                             sorted(pake.config.node.Aliens(test_node_root).urls()))
+        pake.config.node.Aliens(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testListingAliens(self):
+        helpers.gennode(testdir)
+        # test logic
+        foo = {'url': 'http://alien.example.com', 'mirrors': [], 'meta': {}}
+        bar = {'url': 'http://alien.example.net', 'mirrors': [], 'meta': {}}
+        baz = {'url': 'http://alien.example.org', 'mirrors': [], 'meta': {}}
+        pake.config.node.Aliens(test_node_root).set(**foo).set(**bar).set(**baz).write()
+        aliens = pake.config.node.Aliens(test_node_root).all()
+        self.assertIn(foo, aliens)
+        pake.config.node.Aliens(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testSettingNests(self):
+        helpers.gennode(testdir)
+        # test logic
+        pake.config.node.Nests(root=test_node_root).set('foo', './testdir/.pakenest').write()
+        self.assertEqual('./testdir/.pakenest', pake.config.node.Nests(test_node_root).get('foo'))
+        pake.config.node.Nests(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testRemovingNests(self):
+        helpers.gennode(testdir)
+        # test logic
+        nest = {'name': 'foo', 'path': '~/Dev/foo'}
+        pake.config.node.Nests(test_node_root).set(**nest).write()
+        pake.config.node.Nests(test_node_root).remove('foo').write()
+        self.assertRaises(KeyError, pake.config.node.Nests(test_node_root).get, 'foo')
+        pake.config.node.Nests(test_node_root).reset().write()
+         # cleanup
+        helpers.rmnode(testdir)
+
+    def testGettingPathOfOneNest(self):
+        helpers.gennode(testdir)
+        # test logic
+        foo = {'name': 'foo', 'path': '~/Dev/foo'}
+        bar = {'name': 'bar', 'path': '~/Dev/bar'}
+        baz = {'name': 'baz', 'path': '~/Dev/baz'}
+        pake.config.node.Nests(test_node_root).set(**foo).set(**bar).set(**baz).write()
+        self.assertEqual('~/Dev/foo', pake.config.node.Nests(test_node_root).get('foo'))
+        self.assertEqual('~/Dev/bar', pake.config.node.Nests(test_node_root).get('bar'))
+        self.assertEqual('~/Dev/baz', pake.config.node.Nests(test_node_root).get('baz'))
+        pake.config.node.Nests(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+    def testGettingPathsOfAllNests(self):
+        helpers.gennode(testdir)
+        # test logic
+        foo = {'name': 'foo', 'path': '~/Dev/foo'}
+        bar = {'name': 'bar', 'path': '~/Dev/bar'}
+        baz = {'name': 'baz', 'path': '~/Dev/baz'}
+        pake.config.node.Nests(test_node_root).set(**foo).set(**bar).set(**baz).write()
+        self.assertEqual(['~/Dev/bar', '~/Dev/baz', '~/Dev/foo'], sorted(pake.config.node.Nests(test_node_root).paths()))
+        pake.config.node.Nests(test_node_root).reset().write()
+        # cleanup
+        helpers.rmnode(testdir)
+
+
+# Nest related tests
 class NestManagerTests(unittest.TestCase):
     def testNestManagerDirectoriesWriting(self):
         """This test checks for correct initialization of all required directories.
         """
+        # preparation
+        helpers.gennest(testdir)
         ifstream = open('./env/nest/required/directories.json')
         directories = json.loads(ifstream.read())
         ifstream.close()
+        # test logic
         if VERBOSE: print()
         for d in directories:
             path = os.path.join(test_nest_root, d)
             if VERBOSE: print("'{0}'".format(path))
             self.assertEqual(True, os.path.isdir(path))
+        # cleanup
+        helpers.rmnest(testdir)
 
     def testNestManagerConfigWriting(self):
         """This test checks for correct intialization of all required config files.
         """
+        # preparation
+        helpers.gennest(testdir)
         # (filename, desired_content)
         configs = [ ('meta.json', {}),
                     ('versions.json', []),
                     ('dependencies.json', {}),
                     ('files.json', []),
                     ]
+        # test logic
         if VERBOSE: print()
         for f, desired in configs:
             path = os.path.join(test_nest_root, f)
@@ -135,14 +303,17 @@ class NestManagerTests(unittest.TestCase):
             ifstream = open(path, 'r')
             self.assertEqual(desired, json.loads(ifstream.read()))
             ifstream.close()
+        # cleanup
+        helpers.rmnest(testdir)
 
     def testNestManagerRemovingNode(self):
         """This test checks if node gets correctly deleted.
         """
+        # preparation
+        helpers.gennest(testdir)
+        # test logic & cleanup
         pake.nest.manager.remove(root=testdir)
         self.assertNotIn('.pakenest', os.listdir(testdir))
-
-
 
 
 if __name__ == '__main__':
